5622a9a2b2f895c9779a779e093d85049b491ecf
its.alexito_dev <alexgaro2015@gmail.com>
2023-06-21 13:29:43 +0200

    no admin powershell

--- actualizar_programas.ps1
+++ actualizar_programas.ps1
@@ -1,22 +1,5 @@
-# Verificar si el script se está ejecutando como administrador
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Este script debe ejecutarse como administrador." -ForegroundColor Red
-    pause
-    exit
-}
-
-# Ejecutar 'winget upgrade'
-Write-Host "Ejecutando 'winget upgrade'..." -ForegroundColor Green
+# Comando 'winget upgrade'
 winget upgrade
 
-# Esperar a que finalice la actualización
-Write-Host "Esperando a que finalice la actualización..." -ForegroundColor Green
-Start-Sleep -Seconds 5
-
-# Ejecutar 'winget upgrade --all'
-Write-Host "Ejecutando 'winget upgrade --all'..." -ForegroundColor Green
+# Comando 'winget upgrade --all'
 winget upgrade --all
-
-# Mostrar mensaje de finalización
-Write-Host "¡Actualización completa!" -ForegroundColor Green
-pause
